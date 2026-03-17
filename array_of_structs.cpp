ef4594015906a2509b24647accd8f0f54cc163db
lizette iriarte <lizetteiriarte@gmail.com>
2026-03-17 09:57:48 -0700

    I entered Authors name.

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-//Lizette Iriarte
+//Lizette Iriarte.
 // TODO: Step 1 - Define the struct TemperatureRecord
 // It should contain two integer fields: day and temperature.
 
