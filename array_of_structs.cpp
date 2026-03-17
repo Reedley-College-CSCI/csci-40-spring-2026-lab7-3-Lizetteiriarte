5eb3fd62662ba61dceeddc98cde3353bcf38b6b0
lizette iriarte <lizetteiriarte@gmail.com>
2026-03-17 16:39:43 -0700

    Fixed function prototypes before main and completed step 5

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -12,22 +12,32 @@ struct TemperatureRecord {
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(???); // TODO: Fix the parameters
-void printTemperatures(const ???);
-TemperatureRecord findMin(const ???);
-TemperatureRecord findMax(const ???);
-double findAverage(const ???);
+void readTemperatures(TemperatureRecord records[], int& size); // TODO: Fix the parameters
+void printTemperatures(const TemperatureRecord records[], int size);
+TemperatureRecord findMin(const TemperatureRecord records[], int size);
+TemperatureRecord findMax(const TemperatureRecord records[], int size);
+double findAverage(const TemperatureRecord records[], int size);
 
 int main() {
     TemperatureRecord records[MAX_DAYS]; // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
     
     int size = 0;  // Actual number of records read
-
+    void readTemperatures(TemperatureRecord records[], int& size); // TODO: Fix the parameters
+    void printTemperatures(const TemperatureRecord records[], int size);
+    TemperatureRecord findMin(const TemperatureRecord records[], int size);
+    TemperatureRecord findMax(const TemperatureRecord records[], int size);
+    double findAverage(const TemperatureRecord records[], int size);
     readTemperatures(records, size);// TODO: Step 3 - Call readTemperatures() to load data from file
     
     printTemperatures(records, size); // TODO: Step 4 - Print the temperatures
 
-    // TODO: Step 5 - Compute and display min, max, and average temperature
+    TemperatureRecord min = findMin(records, size);
+    TemperatureRecord max = findMax(records, size);
+    double avg = findAverage(records, size);
+    
+    cout << "Min Temp: Day " << min.day << " = " << min.temperature << endl;
+    cout << "Max Temp: Day " << max.day << " = " << max.temperature << endl;
+    cout << "Average Temp: " << avg << endl; // TODO: Step 5 - Compute and display min, max, and average temperature
 
     return 0;
 }
